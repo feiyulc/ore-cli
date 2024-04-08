520aae237ee2a78cf1d8965de534e72fb7c515ec
feiyulc <84794514+feiyulc@users.noreply.github.com>
2024-04-08 12:15:30 +0800

    Update cu_limits.rs

--- src/cu_limits.rs
+++ src/cu_limits.rs
@@ -1,3 +1,8 @@
-pub const CU_LIMIT_CLAIM: u32 = 11_000;
+pub const CU_LIMIT_REGISTER: u32 = 15_000;
+// pub const CU_LIMIT_CLAIM: u32 = 11_000;
+// pub const CU_LIMIT_TRANSFER: u32 = 30_000; // TODO
+pub const CU_LIMIT_ATA: u32 = 240_000;
 pub const CU_LIMIT_RESET: u32 = 12_200;
 pub const CU_LIMIT_MINE: u32 = 3200;
+// pub const CU_LIMIT_MINE: u32 = 6400;
+pub const CU_LIMIT_CLAIM: u32 = 220_000;
